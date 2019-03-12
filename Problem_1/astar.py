8c5e906fa90db0f0d39cba051d1f00153c6fb6b0
Tariq Zahroof <tzahroof@stanford.edu>
2019-03-12 02:39:18 -0700

    Finished problem 1

--- Problem_1/astar.py
+++ Problem_1/astar.py
@@ -5,152 +5,189 @@ import matplotlib.patches as patches
 # Represents a motion planning problem to be solved using A*
 class AStar(object):
 
-    def __init__(self, statespace_lo, statespace_hi, x_init, x_goal, occupancy, resolution=1):
-        self.statespace_lo = statespace_lo         # state space lower bound (e.g., (-5, -5))
-        self.statespace_hi = statespace_hi         # state space upper bound (e.g., (5, 5))
-        self.occupancy = occupancy                 # occupancy grid
-        self.resolution = resolution               # resolution of the discretization of state space (cell/m)
-        self.x_init = self.snap_to_grid(x_init)    # initial state
-        self.x_goal = self.snap_to_grid(x_goal)    # goal state
+        def __init__(self, statespace_lo, statespace_hi, x_init, x_goal, occupancy, resolution=1):
+                self.statespace_lo = statespace_lo         # state space lower bound (e.g., (-5, -5))
+                self.statespace_hi = statespace_hi         # state space upper bound (e.g., (5, 5))
+                self.occupancy = occupancy                 # occupancy grid
+                self.resolution = resolution               # resolution of the discretization of state space (cell/m)
+                self.x_init = self.snap_to_grid(x_init)    # initial state
+                self.x_goal = self.snap_to_grid(x_goal)    # goal state
 
-        self.closed_set = []    # the set containing the states that have been visited
-        self.open_set = []      # the set containing the states that are condidate for future expension
+                self.closed_set = []    # the set containing the states that have been visited
+                self.open_set = []      # the set containing the states that are condidate for future expension
 
-        self.f_score = {}       # dictionary of the f score (estimated cost from start to goal passing through state)
-        self.g_score = {}       # dictionary of the g score (cost-to-go from start to state)
-        self.came_from = {}     # dictionary keeping track of each state's parent to reconstruct the path
+                self.f_score = {}       # dictionary of the f score (estimated cost from start to goal passing through state)
+                self.g_score = {}       # dictionary of the g score (cost-to-go from start to state)
+                self.came_from = {}     # dictionary keeping track of each state's parent to reconstruct the path
 
-        self.open_set.append(x_init)
-        self.g_score[x_init] = 0
-        self.f_score[x_init] = self.distance(x_init,x_goal)
+                self.open_set.append(x_init)
+                self.g_score[x_init] = 0
+                self.f_score[x_init] = self.distance(x_init,x_goal)
 
-        self.path = None        # the final path as a list of states
+                self.path = None        # the final path as a list of states
 
-    # Checks if a give state is free, meaning it is inside the bounds of the map and
-    # is not inside any obstacle
-    # INPUT: (x)
-    #          x - tuple state
-    # OUTPUT: Boolean True/False
-    def is_free(self, x):
-        if x==self.x_init or x==self.x_goal:
-            return True
-        for dim in range(len(x)):
-            if x[dim] < self.statespace_lo[dim]:
-                return False
-            if x[dim] >= self.statespace_hi[dim]:
-                return False
-        if not self.occupancy.is_free(x):
-            return False
-        return True
+        # Checks if a give state is free, meaning it is inside the bounds of the map and
+        # is not inside any obstacle
+        # INPUT: (x)
+        #          x - tuple state
+        # OUTPUT: Boolean True/False
+        def is_free(self, x):
+                if x==self.x_init or x==self.x_goal:
+                        return True
+                for dim in range(len(x)):
+                        if x[dim] < self.statespace_lo[dim]:
+                                return False
+                        if x[dim] >= self.statespace_hi[dim]:
+                                return False
+                if not self.occupancy.is_free(x):
+                        return False
+                return True
 
-    # computes the euclidean distance between two states
-    # INPUT: (x1, x2)
-    #          x1 - first state tuple
-    #          x2 - second state tuple
-    # OUTPUT: Float euclidean distance
-    def distance(self, x1, x2):
-        return np.linalg.norm(np.array(x1)-np.array(x2))
+        # computes the euclidean distance between two states
+        # INPUT: (x1, x2)
+        #          x1 - first state tuple
+        #          x2 - second state tuple
+        # OUTPUT: Float euclidean distance
+        def distance(self, x1, x2):
+                return np.linalg.norm(np.array(x1)-np.array(x2))
 
-    # returns the closest point on a discrete state grid
-    # INPUT: (x)
-    #          x - tuple state
-    # OUTPUT: A tuple that represents the closest point to x on the discrete state grid
-    def snap_to_grid(self, x):
-        return (self.resolution*round(x[0]/self.resolution), self.resolution*round(x[1]/self.resolution))
+        # returns the closest point on a discrete state grid
+        # INPUT: (x)
+        #          x - tuple state
+        # OUTPUT: A tuple that represents the closest point to x on the discrete state grid
+        def snap_to_grid(self, x):
+                return (self.resolution*round(x[0]/self.resolution), self.resolution*round(x[1]/self.resolution))
 
-    # gets the FREE neighbor states of a given state. Assumes a motion model
-    # where we can move up, down, left, right, or along the diagonals by an
-    # amount equal to self.resolution.
-    # Use self.is_free in order to check if any given state is indeed free.
-    # Use self.snap_to_grid (see above) to ensure that the neighbors you compute
-    # are actually on the discrete grid, i.e., if you were to compute neighbors by
-    # simply adding/subtracting self.resolution from x, numerical error could
-    # creep in over the course of many additions and cause grid point equality
-    # checks to fail. To remedy this, you should make sure that every neighbor is
-    # snapped to the grid as it is computed.
-    # INPUT: (x)
-    #           x - tuple state
-    # OUTPUT: List of neighbors that are free, as a list of TUPLES
-    def get_neighbors(self, x):
-        # TODO: fill me in!
+        # gets the FREE neighbor states of a given state. Assumes a motion model
+        # where we can move up, down, left, right, or along the diagonals by an
+        # amount equal to self.resolution.
+        # Use self.is_free in order to check if any given state is indeed free.
+        # Use self.snap_to_grid (see above) to ensure that the neighbors you compute
+        # are actually on the discrete grid, i.e., if you were to compute neighbors by
+        # simply adding/subtracting self.resolution from x, numerical error could
+        # creep in over the course of many additions and cause grid point equality
+        # checks to fail. To remedy this, you should make sure that every neighbor is
+        # snapped to the grid as it is computed.
+        # INPUT: (x)
+        #           x - tuple state
+        # OUTPUT: List of neighbors that are free, as a list of TUPLES
+        def get_neighbors(self, x):
 
-    # Gets the state in open_set that has the lowest f_score
-    # INPUT: None
-    # OUTPUT: A tuple, the state found in open_set that has the lowest f_score
-    def find_best_f_score(self):
-        return min(self.open_set, key=lambda x: self.f_score[x])
+            neighbors = []
 
-    # Use the came_from map to reconstruct a path from the initial location
-    # to the goal location
-    # INPUT: None
-    # OUTPUT: A list of tuples, which is a list of the states that go from start to goal
-    def reconstruct_path(self):
-        path = [self.x_goal]
-        current = path[-1]
-        while current != self.x_init:
-            path.append(self.came_from[current])
-            current = path[-1]
-        return list(reversed(path))
+            for i in range(-1, 2):
+                for j in range(-1, 2):
+                    if not(i is 0 and j is 0):
+                        possState = (x[0] + i*self.resolution, x[1] + j*self.resolution)
 
-    # Plots the path found in self.path and the obstacles
-    # INPUT: None
-    # OUTPUT: None
-    def plot_path(self):
-        if not self.path:
-            return
+                        if(self.is_free(possState)):
+                            neighbors.append(possState)
 
-        fig = plt.figure()
+            return neighbors
 
-        self.occupancy.plot(fig.number)
+        # Gets the state in open_set that has the lowest f_score
+        # INPUT: None
+        # OUTPUT: A tuple, the state found in open_set that has the lowest f_score
+        def find_best_f_score(self):
+                return min(self.open_set, key=lambda x: self.f_score[x])
 
-        solution_path = np.array(self.path) * self.resolution
-        plt.plot(solution_path[:,0],solution_path[:,1], color="green", linewidth=2, label="solution path", zorder=10)
-        plt.scatter([self.x_init[0]*self.resolution, self.x_goal[0]*self.resolution], [self.x_init[1]*self.resolution, self.x_goal[1]*self.resolution], color="green", s=30, zorder=10)
-        plt.annotate(r"$x_{init}$", np.array(self.x_init)*self.resolution + np.array([.2, 0]), fontsize=16)
-        plt.annotate(r"$x_{goal}$", np.array(self.x_goal)*self.resolution + np.array([.2, 0]), fontsize=16)
-        plt.legend(loc='upper center', bbox_to_anchor=(0.5, -0.03), fancybox=True, ncol=3)
+        # Use the came_from map to reconstruct a path from the initial location
+        # to the goal location
+        # INPUT: None
+        # OUTPUT: A list of tuples, which is a list of the states that go from start to goal
+        def reconstruct_path(self):
+                path = [self.x_goal]
+                current = path[-1]
+                while current != self.x_init:
+                        path.append(self.came_from[current])
+                        current = path[-1]
+                return list(reversed(path))
 
-        plt.axis('equal')
-        plt.show()
+        # Plots the path found in self.path and the obstacles
+        # INPUT: None
+        # OUTPUT: None
+        def plot_path(self):
+                if not self.path:
+                        return
 
-    # Solves the planning problem using the A* search algorithm. It places
-    # the solution as a list of of tuples (each representing a state) that go
-    # from self.x_init to self.x_goal inside the variable self.path
-    # INPUT: None
-    # OUTPUT: Boolean, True if a solution from x_init to x_goal was found
-    def solve(self):
-        while len(self.open_set)>0:
+                fig = plt.figure()
+
+                self.occupancy.plot(fig.number)
+
+                solution_path = np.array(self.path) * self.resolution
+                plt.plot(solution_path[:,0],solution_path[:,1], color="green", linewidth=2, label="solution path", zorder=10)
+                plt.scatter([self.x_init[0]*self.resolution, self.x_goal[0]*self.resolution], [self.x_init[1]*self.resolution, self.x_goal[1]*self.resolution], color="green", s=30, zorder=10)
+                plt.annotate(r"$x_{init}$", np.array(self.x_init)*self.resolution + np.array([.2, 0]), fontsize=16)
+                plt.annotate(r"$x_{goal}$", np.array(self.x_goal)*self.resolution + np.array([.2, 0]), fontsize=16)
+                plt.legend(loc='upper center', bbox_to_anchor=(0.5, -0.03), fancybox=True, ncol=3)
+
+                plt.axis('equal')
+                plt.show()
+
+        # Solves the planning problem using the A* search algorithm. It places
+        # the solution as a list of of tuples (each representing a state) that go
+        # from self.x_init to self.x_goal inside the variable self.path
+        # INPUT: None
+        # OUTPUT: Boolean, True if a solution from x_init to x_goal was found
+        def solve(self):
+
+            while len(self.open_set)>0:
             # TODO: fill me in!
 
-        return False
+            #g-score = cost (path length)
+            #f-score = cost + heuristic (how to select node to build path)
+
+            #pop the node with the lowest f-score
+                current_node = self.find_best_f_score()
+                self.open_set.remove(current_node)
+                self.closed_set.append(current_node)
+
+                if(current_node == self.x_goal):
+                    self.path = self.reconstruct_path()
+                    return True
+
+
+                for neighbor in self.get_neighbors(current_node):
+
+                    new_cost = self.g_score[current_node] + self.distance(current_node, neighbor)
+
+                    if (neighbor not in self.g_score or new_cost < self.g_score[neighbor]):
+                        if(neighbor not in self.open_set):
+                            self.open_set.append(neighbor)
+
+                        self.g_score[neighbor] = new_cost
+                        self.f_score[neighbor] = new_cost + self.distance(neighbor, self.x_goal)
+                        self.came_from[neighbor] = current_node
+
+            return False
 
 # A 2D state space grid with a set of rectangular obstacles. The grid is fully deterministic
 class DetOccupancyGrid2D(object):
-    def __init__(self, width, height, obstacles):
-        self.width = width
-        self.height = height
-        self.obstacles = obstacles
+        def __init__(self, width, height, obstacles):
+                self.width = width
+                self.height = height
+                self.obstacles = obstacles
 
-    def is_free(self, x):
-        for obs in self.obstacles:
-            inside = True
-            for dim in range(len(x)):
-                if x[dim] < obs[0][dim] or x[dim] > obs[1][dim]:
-                    inside = False
-                    break
-            if inside:
-                return False
-        return True
+        def is_free(self, x):
+                for obs in self.obstacles:
+                        inside = True
+                        for dim in range(len(x)):
+                                if x[dim] < obs[0][dim] or x[dim] > obs[1][dim]:
+                                        inside = False
+                                        break
+                        if inside:
+                                return False
+                return True
 
-    def plot(self, fig_num=0):
-        fig = plt.figure(fig_num)
-        for obs in self.obstacles:
-            ax = fig.add_subplot(111, aspect='equal')
-            ax.add_patch(
-            patches.Rectangle(
-            obs[0],
-            obs[1][0]-obs[0][0],
-            obs[1][1]-obs[0][1],))
+        def plot(self, fig_num=0):
+                fig = plt.figure(fig_num)
+                for obs in self.obstacles:
+                        ax = fig.add_subplot(111, aspect='equal')
+                        ax.add_patch(
+                        patches.Rectangle(
+                        obs[0],
+                        obs[1][0]-obs[0][0],
+                        obs[1][1]-obs[0][1],))
 
 ### TESTING
 
@@ -184,7 +221,7 @@ occupancy = DetOccupancyGrid2D(width, height, obstacles)
 astar = AStar((0, 0), (width, height), x_init, x_goal, occupancy)
 
 if not astar.solve():
-    print "No path found"
-    exit(0)
+        print "No path found"
+        exit(0)
 
 astar.plot_path()
